95811a862edb08240e89b775808da96b4e6b0d80
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-11-27 22:59:37 +0100

    Implementing 'RoadLink.csv' file & changing the functionality

--- best_distance.py
+++ best_distance.py
@@ -1,89 +1,76 @@
 import csv 
 from math import sin, cos, acos, radians
 
+
 def distance(lat1, lon1, lat2, lon2):
-    """
-    Calculate the great-circle distance between two points on the Earth's surface 
-    using the Haversine formula.
-
-    Parameters:
-    - lat1, lon1: Latitude and longitude of the first point in degrees.
-    - lat2, lon2: Latitude and longitude of the second point in degrees.
-
-    Returns:
-    - The distance between the two points in kilometers.
-    """
     inner_part = min(1.0, max(-1.0, sin(radians(lat1)) * sin(radians(lat2)) + cos(radians(lat1)) * cos(radians(lat2)) * cos(radians(lon1) - radians(lon2))))
     return 6373.0 * acos(inner_part)
 
-# Open and read the CSV file containing road node information
-with open("RoadNode.csv", "r") as file:
-    csv_reader = csv.reader(file)
 
-    france_locations = []
-    germany_locations = []
+with open("RoadNode.csv", "r") as node_file:
+    csv_road_node = csv.reader(node_file)
+    with open("RoadLink.csv", "r") as link_file: 
+        csv_road_link = csv.reader(link_file)
 
-    # Iterate through each row in the CSV file
-    for row in csv_reader:
-        # Splitting coordinates represented in scientific notation
-        x_parts = row[7].split("e")
-        y_parts = row[8].split("e")
+        from_to_nodes = []
+        for row_index, row in enumerate(list(csv_road_link)[1:4]):
+            row = row[0].split(";")
+            from_to_nodes.append({
+                "obj_id": int(row[0]),
+                "from_node": int(row[3]),
+                "to_node": int(row[4])
+            })
 
-        # Check if the row corresponds to France or Germany, and if coordinates are valid
-        if (row[9] == "France" or row[9] == "Germany") and (len(x_parts) > 1 and len(y_parts) > 1 and x_parts[0] != 'X' and y_parts[0] != 'X'):
-            x = float(x_parts[0])
-            y = float(y_parts[0])
 
-            # Store the location information in the respective lists
-            if row[9] == "France":
-                france_locations.append({
-                    "name": row[9], 
-                    "id":  row[2], 
-                    "coords": {"x": x, "y": y}
-                })
-            if row[9] == "Germany":
-                germany_locations.append({
-                    "name": row[9], 
-                    "id":  row[2], 
-                    "coords": {"x": x, "y": y}
-                })
 
-    # Calculate the distance between the first locations in France and Germany
-    first_route = distance(
-        france_locations[0]["coords"]["x"], 
-        france_locations[0]["coords"]["y"], 
-        germany_locations[0]["coords"]["x"], 
-        germany_locations[0]["coords"]["y"]
-    )
 
-    # Initialize the most optimal route with the distance and corresponding node IDs
-    most_optimal_route = {
-        "distance": first_route, 
-        "france_id": france_locations[0]["id"], 
-        "germany_id": germany_locations[0]["id"]
-    } 
+        fastest_destination = {
+            "obj_id": from_to_nodes[0]["obj_id"], 
+            "distance": 1047.63689283956,    
+        }
 
-    # Iterate through all combinations of France and Germany locations to find the most optimal route
-    for fl_index, fl in enumerate(france_locations):
-        for gl_index, gl in enumerate(germany_locations):
-            current_route = distance(
-                france_locations[fl_index]["coords"]["x"], 
-                france_locations[fl_index]["coords"]["y"], 
-                germany_locations[gl_index]["coords"]["x"], 
-                germany_locations[gl_index]["coords"]["y"]
-            )
+        # print(from_to_nodes[0]["obj_id"])
 
-            # Print the comparison between the current route and the most optimal route
-            print(f"MOR {most_optimal_route['distance']} & CR {current_route}")
+        for node_index, node in enumerate(from_to_nodes):
+            from_node_id = from_to_nodes[node_index]["from_node"]
+            to_node_id = from_to_nodes[node_index]["to_node"]
 
-            # Update the most optimal route if the current route is shorter
-            if current_route < most_optimal_route["distance"]:
-                most_optimal_route = {
-                    "distance": current_route, 
-                    "france_id": france_locations[fl_index]["id"], 
-                    "germany_id": germany_locations[gl_index]["id"]
-                }
+            for road_node in list(csv_road_node)[1:]:
 
-    # Print the information about the most optimal route
-    print("Most optimal route:")
-    print(f"Distance: {most_optimal_route['distance']}\nFrance ID: {most_optimal_route['france_id']}\nGermany ID: {most_optimal_route['germany_id']}")
+                x_part = road_node[7].split("e")[0]
+                y_part = road_node[8].split("e")[0]
+
+                # print(list(road_node)[0])
+
+                if int(list(road_node)[0]) == from_node_id:
+                    from_node = { 
+                        "x": float(x_part), 
+                        "y": float(y_part) 
+                    }
+1q
+                elif int(list(road_node)[0]) == to_node_id:
+                    to_node = { 
+                        "x": float(x_part), 
+                        "y": float(y_part) 
+                    }
+
+                    print("from_node")
+                    print(from_node)
+
+                    distance_calculations = distance(
+                        from_node["x"],
+                        to_node["x"],
+                        from_node["y"],
+                        to_node["y"],
+                    )
+
+                    if distance_calculations  < fastest_destination["distance"]:
+                        fastest_destination = { 
+                            "obj_id": node["obj_id"], 
+                            "distance": distance_calculations,     
+                        }
+
+
+        # print(fastest_destination)    
+                    
+            
